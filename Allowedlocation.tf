bbb1437d50a1dda4777c637a7c691f16d73fb3d8
ckharabe498 <84906126+ckharabe498@users.noreply.github.com>
2021-06-21 17:10:17 +0530

    Add files via upload

---
+++ Allowedlocation.tf
@@ -0,0 +1,73 @@
+data "azurerm_subscription" "current" {}
+
+resource "azurerm_policy_definition" "Allowedlocationdef" {
+  name         = "AllowedLocationdefinition"
+  policy_type  = "Custom"
+  mode         = "All"
+  display_name = "CF:Allowed location North Europe and West Europe"
+
+  policy_rule = <<POLICY_RULE
+    {
+    "if": {
+        "allOf": [
+          {
+            "field": "location",
+            "notIn": "[parameters('listOfAllowedLocations')]"
+          },
+          {
+            "field": "location",
+            "notEquals": "global"
+          },
+          {
+            "field": "type",
+            "notEquals": "Microsoft.AzureActiveDirectory/b2cDirectories"
+          }
+        ]
+      },
+      "then": {
+        "effect": "deny"
+        }
+      }
+POLICY_RULE
+
+
+  parameters = <<PARAMETERS
+    {
+    "listOfAllowedLocations": {
+      "type": "Array",
+      "metadata": {
+        "description": "The list of allowed locations for resources.",
+        "displayName": "Allowed locations",
+        "strongType": "location"
+      }
+    }
+  }
+PARAMETERS
+
+}
+
+
+resource "azurerm_policy_assignment" "Allowedlocationpolicyassignment" {
+ name                 = "Allowedlocationpolicyassignment"
+  scope                = data.azurerm_subscription.current.id
+  policy_definition_id = azurerm_policy_definition.Allowedlocationdef.id
+  description          = "Allowed location North europe and West Europe"
+  display_name         = "CF:Allowed Location Policy Assignment"
+
+  metadata = <<METADATA
+    {
+    "category": "General"
+    }
+METADATA
+
+  parameters = <<PARAMETERS
+{
+  "listOfAllowedLocations": {
+    "value": [ 
+        "West Europe",
+        "North Europe"
+         ]
+  }
+}
+PARAMETERS
+}
---
+++ Loganalytics.tf
@@ -0,0 +1,88 @@
+#data "azurerm_subscription" "current" {}
+
+resource "azurerm_policy_definition" "LogAnalyticsAgentVM" {
+  name         = "LogAnalyticsAgentVM"
+  policy_type  = "Custom"
+  mode         = "Indexed"
+  description = " This policy audits any Windows/Linux virtual machines if the Log Analytics agent is not installed."
+  display_name = "CF:The Log Analytics agent should be installed on virtual machines"
+  policy_rule = <<POLICY_RULE
+   {
+    "if": {
+        "field": "type",
+        "equals": "Microsoft.Compute/virtualMachines"
+      },
+      "then": {
+        "effect": "[parameters('effect')]",
+        "details": {
+          "type": "Microsoft.Compute/virtualMachines/extensions",
+          "existenceCondition": {
+            "allOf": [
+              {
+                "field": "Microsoft.Compute/virtualMachines/extensions/publisher",
+                "equals": "Microsoft.EnterpriseCloud.Monitoring"
+              },
+              {
+                "field": "Microsoft.Compute/virtualMachines/extensions/type",
+                "in": [
+                  "MicrosoftMonitoringAgent",
+                  "OmsAgentForLinux"
+                ]
+              },
+              {
+                "field": "Microsoft.Compute/virtualMachines/extensions/provisioningState",
+                "equals": "Succeeded"
+              },
+              {
+                "field": "Microsoft.Compute/virtualMachines/extensions/settings.workspaceId",
+                "exists": "true"
+              }
+            ]
+          }
+        }
+      }
+    }
+POLICY_RULE
+
+
+  parameters = <<PARAMETERS
+    {
+      "effect": {
+        "type": "String",
+        "metadata": {
+          "displayName": "Effect",
+          "description": "Enable or disable the execution of the policy"
+        },
+        "allowedValues": [
+          "AuditIfNotExists",
+          "Disabled"
+        ],
+        "defaultValue": "AuditIfNotExists"
+      }
+    }
+PARAMETERS
+
+}
+
+
+resource "azurerm_policy_assignment" "LogAnalyticsVMagentassignment" {
+name                 = "LogAnalyticsVMagentassignment"
+  scope                = data.azurerm_subscription.current.id
+  policy_definition_id = azurerm_policy_definition.LogAnalyticsAgentVM.id
+  description          = "CF:The Log Analytics agent should be installed on virtual machines"
+  display_name         = "CF:The Log Analytics agent should be installed on virtual machines"
+
+  metadata = <<METADATA
+    {
+    "category": "Monitoring"
+    }
+METADATA
+
+/* parameters = <<PARAMETERS
+{
+  "effect": {
+    "value": [ "AuditIfNotExists"]
+  }
+}
+PARAMETERS */
+}
