5b6f0c5fffedea7b02f34f07bf3ba6203a2758f0
ckharabe498 <84906126+ckharabe498@users.noreply.github.com>
2021-06-21 17:48:34 +0530

    Update Allowedlocation.tf

--- Allowedlocation.tf
+++ Allowedlocation.tf
@@ -1,3 +1,11 @@
+provider "azurerm" {
+client_id = var.client_id
+client_secret = var.client_secret
+subscription_id = var.subscription_id
+tenant_id = var.tenant_id
+
+features {}
+}
 data "azurerm_subscription" "current" {}
 
 resource "azurerm_policy_definition" "Allowedlocationdef" {
