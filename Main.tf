24d5e9811ea58296f4887ebdce3b7f18c081a35e
ckharabe498 <84906126+ckharabe498@users.noreply.github.com>
2021-06-25 15:06:07 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -12,3 +12,11 @@ features {}
   location = "WestEurope"
 }
 
+resource "azurerm_storage_account" "storage1" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.region
+  account_tier             = var.tier
+  account_replication_type = var.account_replication_type
+  tags = var.tags
+}
