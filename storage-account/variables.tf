ac42ee79dd29924b201deea16ea6f1fab72aca60
ckharabe498 <84906126+ckharabe498@users.noreply.github.com>
2021-06-25 23:49:13 +0530

    Add files via upload

---
+++ storage-account/variables.tf
@@ -0,0 +1,23 @@
+variable "storage_account_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "tier" {
+  type = string
+}
+
+variable "account_replication_type" {
+  type = string
+}
+
+variable "tags" {
+  type = map
+}
