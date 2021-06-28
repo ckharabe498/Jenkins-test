53d714b30f1520fdeafc6ba3ce2c71d7d3741bd9
ckharabe498 <84906126+ckharabe498@users.noreply.github.com>
2021-06-28 17:57:07 +0530

    Update storage-account.tf

--- storage-account/storage-account.tf
+++ storage-account/storage-account.tf
@@ -1,9 +1,11 @@
 
 resource "azurerm_storage_account" "storage" {
-  name                     = var.storage_account_name
-  resource_group_name      = var.resource_group_name
-  location                 = var.region
-  account_tier             = var.tier
-  account_replication_type = var.account_replication_type
-  tags = var.tags
+  name                     = "test-storage"
+  resource_group_name      = "yogesh"
+  location                 = "West Europe"
+  account_tier             = "standerd"
+  account_replication_type = "lsr"
+  tags = {
+    env= "prod"
+  }
 }
