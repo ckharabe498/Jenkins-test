a4c9b9df1257d475fcf31ec21d4d5b2e47bd28d8
ckharabe498 <84906126+ckharabe498@users.noreply.github.com>
2021-06-28 19:13:39 +0530

    Update main.tf

--- RG/main.tf
+++ RG/main.tf
@@ -1,11 +1,3 @@
-provider "azurerm" {
-client_id = var.client_id
-client_secret = var.client_secret
-subscription_id = var.subscription_id
-tenant_id = var.tenant_id
-
-features {}
-}
 
 resource "azurerm_resource_group" "rg" {
     for_each              =   var.resource_group
